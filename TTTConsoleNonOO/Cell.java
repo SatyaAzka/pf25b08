67b0358ee4542b3d4c23acf8c75084f6ffc7fbf2
FahmiGF <fahmige123@gmail.com>
2025-06-10 23:20:39 +0800

    Testing

--- TTTConsoleNonOO/Cell.java
+++ TTTConsoleNonOO/Cell.java
@@ -4,19 +4,25 @@
 public class Cell {  // save as "Cell.java"
     // Define properties (package-visible)
     /** Content of this cell (CROSS, NOUGHT, NO_SEED) */
-    Seed content;
+    private Seed content;
     /** Row and column of this cell, not used in this program */
     int row, col;
 
     /** Constructor to initialize this cell */
     public Cell(int row, int col) {
-        this.row = row;
-        this.col = col;
         this.content = Seed.NO_SEED;
     }
 
     /** Reset the cell content to EMPTY, ready for a new game. */
     public void newGame() {
+        this.content = Seed.NO_SEED;
+    }
+
+    public Seed getContent() {
+        return content;
+    }
+
+    public void clear() {
         this.content = Seed.NO_SEED;
     }
 
