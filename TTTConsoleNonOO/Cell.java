94aa6f3fd2f6c9b70d0343206f94877f5e897d9f
FahmiGF <fahmige123@gmail.com>
2025-06-11 00:07:44 +0800

    Adding graphics on the tictactoe and enumeration applied

--- TTTConsoleNonOO/Cell.java
+++ TTTConsoleNonOO/Cell.java
@@ -4,25 +4,19 @@
 public class Cell {  // save as "Cell.java"
     // Define properties (package-visible)
     /** Content of this cell (CROSS, NOUGHT, NO_SEED) */
-    private Seed content;
+    Seed content;
     /** Row and column of this cell, not used in this program */
     int row, col;
 
     /** Constructor to initialize this cell */
     public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
         this.content = Seed.NO_SEED;
     }
 
     /** Reset the cell content to EMPTY, ready for a new game. */
     public void newGame() {
-        this.content = Seed.NO_SEED;
-    }
-
-    public Seed getContent() {
-        return content;
-    }
-
-    public void clear() {
         this.content = Seed.NO_SEED;
     }
 
