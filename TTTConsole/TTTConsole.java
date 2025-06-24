73a650affe0a9c7906a8465c15fb414dea6322b4
SatyaAzka <Fahmige123@gmail.com>
2025-06-24 19:30:14 +0800

    Tambahan Seluruh Current temporary file

--- TTTConsole/TTTConsole.java
+++ TTTConsole/TTTConsole.java
@@ -1,12 +1,10 @@
-package TTTConsoleNonOOV1_5_6;
-
 import java.util.Scanner;
 /**
  * Tic-Tac-Toe: Two-player, console-based, non-graphics, non-OO version.
  * All variables/methods are declared as static (i.e., class)
  *  in this non-OO version.
  */
-public class TTTConsoleOO {
+public class TTTConsole {
     // Define named constants for:
     //  1. Player: using CROSS and NOUGHT
     //  2. Cell contents: using CROSS, NOUGHT and NO_SEED
--- TTTConsole/TTTGraphics.java
+++ TTTConsole/TTTGraphics.java
@@ -8,8 +8,8 @@ public class TTTGraphics extends JFrame {
    private static final long serialVersionUID = 1L; // to prevent serializable warning
 
    // Define named constants for the game board
-   public static final int ROWS = 6;  // ROWS x COLS cells
-   public static final int COLS = 7;
+   public static final int ROWS = 3;  // ROWS x COLS cells
+   public static final int COLS = 3;
 
    // Define named constants for the drawing graphics
    public static final int CELL_SIZE = 120; // cell width/height (square)
@@ -68,7 +68,7 @@ public class TTTGraphics extends JFrame {
 
             if (currentState == State.PLAYING) {
                if (row >= 0 && row < ROWS && col >= 0
-                     && col < COLS && board[row][col] == Seed.NO_SEED) {
+                       && col < COLS && board[row][col] == Seed.NO_SEED) {
                   // Update board[][] and return the new game state after the move
                   currentState = stepGame(currentPlayer, row, col);
                   // Switch player
@@ -130,19 +130,19 @@ public class TTTGraphics extends JFrame {
 
       // Compute and return the new game state
       if (board[selectedRow][0] == player  // 3-in-the-row
-                && board[selectedRow][1] == player
-                && board[selectedRow][2] == player
-             || board[0][selectedCol] == player // 3-in-the-column
-                && board[1][selectedCol] == player
-                && board[2][selectedCol] == player
-             || selectedRow == selectedCol  // 3-in-the-diagonal
-                && board[0][0] == player
-                && board[1][1] == player
-                && board[2][2] == player
-             || selectedRow + selectedCol == 2 // 3-in-the-opposite-diagonal
-                && board[0][2] == player
-                && board[1][1] == player
-                && board[2][0] == player) {
+              && board[selectedRow][1] == player
+              && board[selectedRow][2] == player
+              || board[0][selectedCol] == player // 3-in-the-column
+              && board[1][selectedCol] == player
+              && board[2][selectedCol] == player
+              || selectedRow == selectedCol  // 3-in-the-diagonal
+              && board[0][0] == player
+              && board[1][1] == player
+              && board[2][2] == player
+              || selectedRow + selectedCol == 2 // 3-in-the-opposite-diagonal
+              && board[0][2] == player
+              && board[1][1] == player
+              && board[2][0] == player) {
          return (player == Seed.CROSS) ? State.CROSS_WON : State.NOUGHT_WON;
       } else {
          // Nobody win. Check for DRAW (all cells occupied) or PLAYING.
@@ -172,18 +172,18 @@ public class TTTGraphics extends JFrame {
          g.setColor(COLOR_GRID);
          for (int row = 1; row < ROWS; ++row) {
             g.fillRoundRect(0, CELL_SIZE * row - GRID_WIDTH_HALF,
-                  BOARD_WIDTH-1, GRID_WIDTH, GRID_WIDTH, GRID_WIDTH);
+                    BOARD_WIDTH-1, GRID_WIDTH, GRID_WIDTH, GRID_WIDTH);
          }
          for (int col = 1; col < COLS; ++col) {
             g.fillRoundRect(CELL_SIZE * col - GRID_WIDTH_HALF, 0,
-                  GRID_WIDTH, BOARD_HEIGHT-1, GRID_WIDTH, GRID_WIDTH);
+                    GRID_WIDTH, BOARD_HEIGHT-1, GRID_WIDTH, GRID_WIDTH);
          }
 
          // Draw the Seeds of all the cells if they are not empty
          // Use Graphics2D which allows us to set the pen's stroke
          Graphics2D g2d = (Graphics2D)g;
          g2d.setStroke(new BasicStroke(SYMBOL_STROKE_WIDTH,
-               BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                 BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
          for (int row = 0; row < ROWS; ++row) {
             for (int col = 0; col < COLS; ++col) {
                int x1 = col * CELL_SIZE + CELL_PADDING;
