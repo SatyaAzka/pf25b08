a262dfff7fdbb9c86bd6090e01a51650b18a9a45
FahmiGF <fahmige123@gmail.com>
2025-06-25 22:01:28 +0800

    (Referensi) Jika nantinya dikembangkan lagi jadi 6 x 7 (Connect four). Tapi fokus 3x3

--- TTTConsoleNonOO/Board.java
+++ TTTConsoleNonOO/Board.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 public class Board {
    // Define named constants
-   public static final int ROWS = 3;  // ROWS x COLS cells
-   public static final int COLS = 3;
+   public static final int ROWS = 6;  // ROWS x COLS cells
+   public static final int COLS = 7;
    // Define named constants for drawing
    public static final int CANVAS_WIDTH = Cell.SIZE * COLS;  // the drawing canvas
    public static final int CANVAS_HEIGHT = Cell.SIZE * ROWS;
@@ -21,7 +21,7 @@ public class Board {
       cells = new Cell[ROWS][COLS]; // allocate the array
       for (int row = 0; row < ROWS; ++row) {
          for (int col = 0; col < COLS; ++col) {
-            // Allocate element of the array
+
             cells[row][col] = new Cell(row, col);
             // Cells are initialized in the constructor
          }
