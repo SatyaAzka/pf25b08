e7ea896050e8df56c72af87fc83056f0bc5cc154
Satya Azka Aqilla <satya.azka.a@gmail.com>
2025-06-25 12:10:43 +0800

    AI Player dan Board diperbaiki, ditambahkan diff easy and hard

--- TTTConsoleNonOO/Board.java
+++ TTTConsoleNonOO/Board.java
@@ -1,7 +1,4 @@
 import java.awt.*;
-/**
- * The Board class models the ROWS-by-COLS game board.
- */
 public class Board {
    // Define named constants
    public static final int ROWS = 3;  // ROWS x COLS cells
@@ -14,16 +11,12 @@ public class Board {
    public static final Color COLOR_GRID = Color.LIGHT_GRAY;  // grid lines
    public static final int Y_OFFSET = 1;  // Fine tune for better display
 
-   // Define properties (package-visible)
-   /** Composes of 2D array of ROWS-by-COLS Cell instances */
    Cell[][] cells;
 
-   /** Constructor to initialize the game board */
    public Board() {
       initGame();
    }
 
-   /** Initialize the game objects (run once) */
    public void initGame() {
       cells = new Cell[ROWS][COLS]; // allocate the array
       for (int row = 0; row < ROWS; ++row) {
@@ -35,7 +28,6 @@ public class Board {
       }
    }
 
-   /** Reset the game board, ready for new game */
    public void newGame() {
       for (int row = 0; row < ROWS; ++row) {
          for (int col = 0; col < COLS; ++col) {
@@ -44,47 +36,37 @@ public class Board {
       }
    }
 
-   /**
-    *  The given player makes a move on (selectedRow, selectedCol).
-    *  Update cells[selectedRow][selectedCol]. Compute and return the
-    *  new game state (PLAYING, DRAW, CROSS_WON, NOUGHT_WON).
-    */
    public State stepGame(Seed player, int selectedRow, int selectedCol) {
-      // Update game board
       cells[selectedRow][selectedCol].content = player;
 
-      // Compute and return the new game state
-      if (cells[selectedRow][0].content == player  // 3-in-the-row
+      if (cells[selectedRow][0].content == player
               && cells[selectedRow][1].content == player
               && cells[selectedRow][2].content == player
-              || cells[0][selectedCol].content == player // 3-in-the-column
+              || cells[0][selectedCol].content == player
               && cells[1][selectedCol].content == player
               && cells[2][selectedCol].content == player
-              || selectedRow == selectedCol     // 3-in-the-diagonal
+              || selectedRow == selectedCol
               && cells[0][0].content == player
               && cells[1][1].content == player
               && cells[2][2].content == player
-              || selectedRow + selectedCol == 2 // 3-in-the-opposite-diagonal
+              || selectedRow + selectedCol == 2
               && cells[0][2].content == player
               && cells[1][1].content == player
               && cells[2][0].content == player) {
          return (player == Seed.CROSS) ? State.CROSS_WON : State.NOUGHT_WON;
       } else {
-         // Nobody win. Check for DRAW (all cells occupied) or PLAYING.
          for (int row = 0; row < ROWS; ++row) {
             for (int col = 0; col < COLS; ++col) {
                if (cells[row][col].content == Seed.NO_SEED) {
-                  return State.PLAYING; // still have empty cells
+                  return State.PLAYING;
                }
             }
          }
-         return State.DRAW; // no empty cell, it's a draw
+         return State.DRAW;
       }
    }
 
-   /** Paint itself on the graphics canvas, given the Graphics context */
    public void paint(Graphics g) {
-      // Draw the grid-lines
       g.setColor(COLOR_GRID);
       for (int row = 1; row < ROWS; ++row) {
          g.fillRoundRect(0, Cell.SIZE * row - GRID_WIDTH_HALF,
@@ -96,8 +78,6 @@ public class Board {
                  GRID_WIDTH, CANVAS_HEIGHT - 1,
                  GRID_WIDTH, GRID_WIDTH);
       }
-
-      // Draw all the cells
       for (int row = 0; row < ROWS; ++row) {
          for (int col = 0; col < COLS; ++col) {
             cells[row][col].paint(g);  // ask the cell to paint itself
